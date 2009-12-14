d4b8720448eafe0a4898ad42bf3d31af5a5b24b5
Daniel Nelson <daniel@Optiplex760.(none)>
2009-12-14 10:08:39 -0600

    fixed command line

--- bin/import_vectors.rb
+++ bin/import_vectors.rb
@@ -1,8 +1,9 @@
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 require 'prawn_vector_import'
 
-if ARGV[0].blank? || ARGV[1].blank?
-  puts 'Usage: ruby bin/import_vectors.rb path_to_ai_or_pdf_file method_name > method_name.rb'
+if ARGV[0].nil? || ARGV[1].nil?
+  puts "\nUsage: ruby bin/import_vectors.rb path_to_ai_or_pdf_file method_name > method_name.rb\n\n"
+else
+  import = PrawnVectorImport::Import.new(ARGV[0], ARGV[1])
+  puts import.output
 end
-import = PrawnVectorImport::Import.new(ARGV[0], ARGV[1])
-puts import.output
